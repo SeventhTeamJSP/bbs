f4b3208d98f6dad199519b452c6e104fbf3538f7
godbang <1322407800@qq.com>
2019-01-03 19:01:09 +0800

    make some change by zzb

--- src/main/java/com/nediiii/ncu/bbs/controller/BlockController.java
+++ src/main/java/com/nediiii/ncu/bbs/controller/BlockController.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+/**
+ *  控制器，restful风格opi
+ */
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
